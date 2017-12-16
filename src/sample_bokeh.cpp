f60df0f94e4a899c38e18e8b5daceb09488eed4c
Zeno Pelgrims <zenopelgrims@gmail.com>
2017-12-16 15:26:10 +0100

    cimg working, sample coordinates ok

--- src/sample_bokeh.cpp
+++ src/sample_bokeh.cpp
@@ -1,14 +1,14 @@
 #include <ai.h>
+#include <math.h>
 #include "../include/lens.h"
 
 
 #define cimg_display 0
-#include "paper/include/CImg.h"
-#include "paper/include/spectrum.h"
+#include "../include/CImg.h"
 using namespace cimg_library;
 
 
-// need to pass the lens data to this shader
+// need to pass the lens data to this shader, probably have to do this in a global structure (gael honorez)
 // check for intersections along P->Lens path
 
 AI_SHADER_NODE_EXPORT_METHODS(SampleBokehMtd);
@@ -16,10 +16,7 @@ AI_SHADER_NODE_EXPORT_METHODS(SampleBokehMtd);
 struct SampleBokehData
 {
    AtString aov_name;
-
-   float aperture_radius;
-
-   CImg<float> img_out;
+   CImg<float> * img_out;
 
 };
  
@@ -38,8 +35,6 @@ inline uint32_t xor128(void){
     x = y; y = z; z = w;
     return w = (w ^ (w >> 19) ^ t ^ (t >> 8));
 }
-
-
 
 
 // Improved concentric mapping code by Dave Cline [peter shirley´s blog]
@@ -66,24 +61,19 @@ inline void concentric_disk_sample(float ox, float oy, AtVector2 *lens)
 }
 
 
-
-
 // given camera space scene point, return point on sensor
-void trace_backwards(const AtVector sample_position, const float aperture_radius, const float lambda, AtVector2 &sensor_position)
+inline void trace_backwards(const AtVector sample_position, const float aperture_radius, const float lambda, AtVector2 &sensor_position)
 {
    const float target[3] = { sample_position.x, sample_position.y, sample_position.z};
 
    // initialize 5d light fields
    float sensor[5] = {0.0f};
    float out[5] = {0.0f};
+   float aperture[2] = {0.0f};
    sensor[4] = lambda;
-   int count = 0;
-
-   // just point through center of aperture
-   float aperture[2] = {0.0f, 0.0f};
 
    AtVector2 lens;
-   concentricDiskSample(xor128() / 4294967296.0f, xor128() / 4294967296.0f, &lens);
+   concentric_disk_sample(xor128() / 4294967296.0f, xor128() / 4294967296.0f, &lens);
    aperture[0] = lens.x * aperture_radius;
    aperture[1] = lens.y * aperture_radius;
 
@@ -91,11 +81,6 @@ void trace_backwards(const AtVector sample_position, const float aperture_radius
 
    sensor_position.x = sensor[0];
    sensor_position.y = sensor[1];
-   /*
-   if(sensor[2+k] > 0){
-      offset = sensor[k]/sensor[2+k];
-   }
-   */
 }
 
 
@@ -120,14 +105,40 @@ node_update
    data->aov_name = AiNodeGetStr(node, "aov_name");
    AiAOVRegister(data->aov_name, AI_TYPE_RGBA, AI_AOV_BLEND_OPACITY);
 
-   // get camera data
-   // data->aperture_radius = 
-   data->img_out = new CImg<float>(width, height, 1, 3, 0);
+   // camera data should be updated globally, no need to re-assign
+
+   
+   float xres = AiNodeGetInt(AiUniverseGetOptions(), "xres");
+   float yres = AiNodeGetInt(AiUniverseGetOptions(), "yres");
+   data->img_out = new CImg<float>(xres, yres, 1, 4, 0);
+
+   
 }
  
 node_finish
 {
    SampleBokehData *data = (SampleBokehData*)AiNodeGetLocalData(node);
+
+   float xres = AiNodeGetInt(AiUniverseGetOptions(), "xres");
+   float yres = AiNodeGetInt(AiUniverseGetOptions(), "yres");
+
+   const int samples = 1000;
+   
+   // normalize samples
+   for (int j = 0; j < yres; ++j)
+   {
+     for (int i = 0; i < xres; ++i)
+     { 
+         data->img_out->atXY(i,j,0,0) /= samples;
+         data->img_out->atXY(i,j,0,1) /= samples;
+         data->img_out->atXY(i,j,0,2) /= samples;
+         data->img_out->atXY(i,j,0,3) /= samples;
+     } 
+   }
+
+   data->img_out->save("/Users/zeno/pota/tests/image/cimg_bokeh.ppm");
+   
+
    delete data;
 }
  
@@ -135,71 +146,97 @@ shader_evaluate
 {
    const SampleBokehData *data = (SampleBokehData*)AiNodeGetLocalData(node);
 
-   // figure out better way, based on:
-   // distance from focus point
-   // intensity of sample
-   if (sg->out.RGBA().r > 4.0f || sg->out.RGBA().g > 4.0f || sg->out.RGBA().b > 4.0f)
-   {
-
-      AtRGBA sample_energy = sg->out.RGBA / samples;
-
-      // convert sample world space position to camera space
-      AtMatrix world_to_camera_matrix;
-      AtVector screen_space_sample_position;
-      AtVector2 sensor_position;
-
-      AiWorldToCameraMatrix(AiUniverseGetCamera(), sg->time, world_to_camera_matrix);
-      AiM4PointByMatrixMult(&screen_space_sample_position, world_to_camera_matrix, &sg->P);
-
-      // do i need to care about the w vector element? As in Marc's shader?
-
-
-      //trace backwards
-      for(count=0; count<samples; count++)
-      {
-
-         trace_backwards(screen_space_sample_position, data->aperture_radius, data->lambda, sensor_position);
-         
-         // do i need to check for pupil intersections or not?
-
-         // what is the pixel position? Convert sensor position to pixel position
-         // also not sure about units of sensor_width here
-         AtVector2 pixel(sensor_coords[0] / (data->sensor_width * 0.5), sensor_coords[1] / (data->sensor_width * 0.5));
-
-         //now i have sx, sy, but how to convert these to x, y?
-         // screen-space coordinates will range between
-         // (screen_window_min.x, screen_window_min.y/frame_aspect_ratio) and
-         // (screen_window_max.x, screen_window_max.y/frame_aspect_ratio) 
-
-         // write sample to image
-         data->img_out->atXY(pixel.x, pixel.y, 0, 0) = sample_energy.r;
-         data->img_out->atXY(pixel.x, pixel.y, 0, 1) = sample_energy.g;
-         data->img_out->atXY(pixel.x, pixel.y, 0, 2) = sample_energy.b;
-      }
-
-      // print sample positions as test
-      AiMsgInfo("[POTA] flagged sample position: [%f, %f, %f]", sg->P.x, sg->P.y, sg->P.z);
-   }
- 
-
-
-
-
    // write AOV only if in use
    // should i be putting everthing inside this loop?
    if ((sg->Rt & AI_RAY_CAMERA) && AiAOVEnabled(data->aov_name, AI_TYPE_RGBA))
    {
-      AiAOVSetRGBA(sg, data->aov_name, sample_r_intensity);
-   }
+
+      const float aperture_radius = 12.75;
+      const float lambda = 0.55f;
+      const float sensor_width = 36.0;
+      const float xres = AiNodeGetInt(AiUniverseGetOptions(), "xres");
+      const float yres = AiNodeGetInt(AiUniverseGetOptions(), "yres");
+
+      const int samples = 1000;
+      const float minimum_rgb = 17.0f;
+
+      AtRGBA sample_energy(0.0, 0.0, 0.0, 0.0);
+
+      // figure out better way, based on:
+      // distance from focus point
+      // intensity of sample
+      if ((sg->out.RGBA().r > minimum_rgb) || (sg->out.RGBA().g > minimum_rgb) || (sg->out.RGBA().b > minimum_rgb))
+      {
+         
+         sample_energy.r = sg->out.RGBA().r;
+         sample_energy.g = sg->out.RGBA().g;
+         sample_energy.b = sg->out.RGBA().b;
+         sample_energy.a = 1.0f;
+
    
-   // probably have to write to separate image buffer instead, not sure if i can write to different pixels..
-   //sg->out.RGBA() = sample_r_intensity;
+         // convert sample world space position to camera space
+         AtMatrix world_to_camera_matrix;
+         AtVector2 sensor_position;
+
+         AiWorldToCameraMatrix(AiUniverseGetCamera(), sg->time, world_to_camera_matrix);
+         AtVector camera_space_sample_position = AiM4PointByMatrixMult(world_to_camera_matrix, sg->P);
+
+         //AiMsgInfo("[POTA] sample camera space pos: [%f, %f, %f]", camera_space_sample_position.x, camera_space_sample_position.y, camera_space_sample_position.z);
+         // do i need to care about the w vector element? As in Marc's shader?
+
+         
+         //trace backwards
+         for(int count=0; count<samples; count++)
+         {
+            // is the sensor at z0? I think first lens element is instead. Might have to subtract lens length? not sure
+            trace_backwards(camera_space_sample_position, aperture_radius, lambda, sensor_position);
+            
+            AiMsgInfo("sensor_position: [%f, %f]", sensor_position.x, sensor_position.y);
+
+            // do i need to check for pupil intersections or not?
+
+            // what is the pixel position? Convert sensor position to pixel position
+            // also not sure about units of sensor_width here
+            AtVector2 s(sensor_position.x / (sensor_width * 0.5), sensor_position.y / (sensor_width * 0.5));
+            //const int pixel_x = (int)round(((s.x + 1)/2.0) * xres);
+            //const int pixel_y = (int)round(((s.y + 1)/2.0) * yres);
+            const float pixel_x = ((s.x + 1)/2.0) * xres;
+            const float pixel_y = ((s.y + 1)/2.0) * yres;
+
+            // something is going wrong with the pixel positions, probably due to sensor_position
+            AiMsgInfo("pixel: [%f, %f]", pixel_x, pixel_y);
+            // screen-space coordinates will range between
+            // (screen_window_min.x, screen_window_min.y/frame_aspect_ratio) and
+            // (screen_window_max.x, screen_window_max.y/frame_aspect_ratio) 
+
+            // write sample to image
+            // think cimg rgb values are (0->255) instead of (0->1)
+
+            //const float rgba[4] = {sample_energy.r, sample_energy.g, sample_energy.b, sample_energy.a};
+            //data->img_out->set_linear_atXY(sample_energy.r, pixel_x, pixel_y, 0, 0, true);
+            //data->img_out->set_linear_atXY(sample_energy.g, pixel_x, pixel_y, 0, 1, true);
+            //data->img_out->set_linear_atXY(sample_energy.b, pixel_x, pixel_y, 0, 2, true);
+            //data->img_out->set_linear_atXY(sample_energy.a, pixel_x, pixel_y, 0, 3, true);
+
+            /*data->img_out->atXY(pixel_x, pixel_y, 0, 0) = sample_energy.r + data->img_out->atXY(pixel_x, pixel_y, 0, 0);
+            data->img_out->atXY(pixel_x, pixel_y, 0, 1) = sample_energy.g + data->img_out->atXY(pixel_x, pixel_y, 0, 1);
+            data->img_out->atXY(pixel_x, pixel_y, 0, 2) = sample_energy.b + data->img_out->atXY(pixel_x, pixel_y, 0, 2);
+            data->img_out->atXY(pixel_x, pixel_y, 0, 3) = sample_energy.a + data->img_out->atXY(pixel_x, pixel_y, 0, 3);
+            */
+         }
+
+         
+      }
+
+      // ideally would be cool to write to an aov but not sure if I can access the different pixels other than
+      // the one related to the current sample
+      // AiAOVSetRGBA(sg, data->aov_name, sample_energy);
+   }
 }
  
 node_loader
 {
-   if (i != 0)
-      return false;
+   if (i != 0) return false;
    node->methods     = SampleBokehMtd;
    node->output_type = AI_TYPE_RGBA;
    node->name        = "sample_bokeh";
