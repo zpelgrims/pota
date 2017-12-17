a4cdcba5d5c5c8f9fb38ed51a929885a606cb92d
Zeno Pelgrims <zenopelgrims@gmail.com>
2017-12-17 12:36:23 +0100

    crude aov output, adding shared header

--- src/pota.cpp
+++ src/pota.cpp
@@ -28,11 +28,6 @@ std::string DRAW_DIRECTORY="/Users/zeno/pota/tests/";
 #endif
 
 
-#ifdef __APPLE__
-	const char emoticon[5] = {0xF0, 0x9F, 0xA5, 0x91, '\0'};
-#else
-	const char emoticon[0] = {};
-#endif
 
 
 AI_CAMERA_NODE_EXPORT_METHODS(potaMethods)
@@ -61,46 +56,6 @@ static const char* LensModelNames[] =
     NULL
 };
 
-
-// xorshift fast random number generator
-inline uint32_t xor128(void){
-    static uint32_t x = 123456789, y = 362436069, z = 521288629, w = 88675123;
-    uint32_t t = x ^ (x << 11);
-    x = y; y = z; z = w;
-    return w = (w ^ (w >> 19) ^ t ^ (t >> 8));
-}
-
-
-
-inline float Lerp(float t, float v1, float v2)
-{
-	return (1 - t) * v1 + t * v2;
-}
-
-
-
-// Improved concentric mapping code by Dave Cline [peter shirley´s blog]
-// maps points on the unit square onto the unit disk uniformly
-inline void concentric_disk_sample(float ox, float oy, AtVector2 *lens)
-{
-    float phi, r;
-
-    // switch coordinate space from [0, 1] to [-1, 1]
-    float a = 2.0 * ox - 1.0;
-    float b = 2.0 * oy - 1.0;
-
-    if ((a * a) > (b * b)){
-        r = a;
-        phi = (0.78539816339f) * (b / a);
-    }
-    else {
-        r = b;
-        phi = (AI_PIOVER2)-(0.78539816339f) * (a / b);
-    }
-
-    lens->x = r * std::cos(phi);
-    lens->y = r * std::sin(phi);
-}
 
 
 
