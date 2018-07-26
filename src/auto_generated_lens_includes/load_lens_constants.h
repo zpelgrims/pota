b3c81fd22d656b051597fd3515f3d343bb461d73
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-07-25 17:34:18 -0700

    was loading free lenses twice, switched to free+commercial

--- src/auto_generated_lens_includes/load_lens_constants.h
+++ src/auto_generated_lens_includes/load_lens_constants.h
@@ -4,6 +4,3 @@
 #ifdef LENS_ID_FREE
 	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/100/code/lens_constants.h"
 #endif
-#ifdef LENS_ID_1
-	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/100/code/lens_constants.h"
-#endif
