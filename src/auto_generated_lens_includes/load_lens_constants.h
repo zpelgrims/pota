e97a987634283857ea1053232c7fbf1c52df8eed
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-08-03 11:56:15 -0700

    adding rear anamorphic

--- src/auto_generated_lens_includes/load_lens_constants.h
+++ src/auto_generated_lens_includes/load_lens_constants.h
@@ -2,5 +2,6 @@
 
 
 #ifdef LENS_ID_FREE
+	#include "../../../polynomial-optics/database/lenses/2006-Panavision-Angenieux-rear-anamorph-double-gauss/100/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/100/code/lens_constants.h"
 #endif
