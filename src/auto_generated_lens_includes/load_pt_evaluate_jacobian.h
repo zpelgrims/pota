a0b78ae11016cfb87a71d39aec855dad0fab49c1
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-08-29 07:47:41 -0700

    adding 1954 cooke anamorphic

--- src/auto_generated_lens_includes/load_pt_evaluate_jacobian.h
+++ src/auto_generated_lens_includes/load_pt_evaluate_jacobian.h
@@ -4,4 +4,5 @@
 #ifdef LENS_ID_FREE
 	#include "../../../polynomial-optics/database/lenses/2006-Panavision-Angenieux-rear-anamorph-double-gauss/100/code/pt_evaluate_jacobian.h"
 	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/100/code/pt_evaluate_jacobian.h"
+	#include "../../../polynomial-optics/database/lenses/1954-cooke-anamorphic/100/code/pt_evaluate_jacobian.h"
 #endif
--- src/auto_generated_lens_includes/pota_cpp_lenses.h
+++ src/auto_generated_lens_includes/pota_cpp_lenses.h
@@ -4,4 +4,5 @@
 #ifdef LENS_ID_FREE
 	"Panavision_Angenieux_rear_anamorph_double_gauss_2006_100mm",
 	"angenieux_double_gauss_1953_100mm",
+	"cooke_anamorphic_1954_100mm",
 #endif
