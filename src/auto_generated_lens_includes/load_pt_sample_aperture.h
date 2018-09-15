5a1fb2bec8762420381ad13ec18db8341a800397
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-09-14 21:27:52 -0700

    cropping bugfix, pota class rename, adding raytraced prototype

--- src/auto_generated_lens_includes/load_pt_sample_aperture.h
+++ src/auto_generated_lens_includes/load_pt_sample_aperture.h
@@ -2,7 +2,5 @@
 
 
 #ifdef LENS_ID_FREE
-	#include "../../../polynomial-optics/database/lenses/2006-Panavision-Angenieux-rear-anamorph-double-gauss/100/code/pt_sample_aperture.h"
 	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/100/code/pt_sample_aperture.h"
-	#include "../../../polynomial-optics/database/lenses/1954-cooke-anamorphic/100/code/pt_sample_aperture.h"
 #endif
--- src/potabokehAOV.cpp
+++ src/potabokehAOV.cpp
@@ -20,7 +20,7 @@ inline void replace_frame_numbering(std::string &original_string){
 }
 
 
-struct PotaBokehAOVData
+struct PotaBokehAOV
 {
   AtString bokeh_aov_name;
   AtString discarded_samples_aov_name;
@@ -50,90 +50,90 @@ node_parameters
  
 node_initialize
 {
-   AiNodeSetLocalData(node, new PotaBokehAOVData());
+   AiNodeSetLocalData(node, new PotaBokehAOV());
 }
 
  
 node_update
 {
-  PotaBokehAOVData *bokeh_data = (PotaBokehAOVData*)AiNodeGetLocalData(node);
-  //const MyCameraData *camera_data = (MyCameraData*)AiNodeGetLocalData(AiUniverseGetCamera());
+  PotaBokehAOV *bokeh = (PotaBokehAOV*)AiNodeGetLocalData(node);
+  //const Camera *camera = (Camera*)AiNodeGetLocalData(AiUniverseGetCamera());
 
   // register AOVs
-  bokeh_data->bokeh_aov_name = AiNodeGetStr(node, "bokeh_aov_name");
-  AiAOVRegister(bokeh_data->bokeh_aov_name, AI_TYPE_RGBA, AI_AOV_BLEND_OPACITY);
-  bokeh_data->discarded_samples_aov_name = AiNodeGetStr(node, "discarded_samples_aov_name");
-  AiAOVRegister(bokeh_data->discarded_samples_aov_name, AI_TYPE_RGBA, AI_AOV_BLEND_OPACITY);
+  bokeh->bokeh_aov_name = AiNodeGetStr(node, "bokeh_aov_name");
+  AiAOVRegister(bokeh->bokeh_aov_name, AI_TYPE_RGBA, AI_AOV_BLEND_OPACITY);
+  bokeh->discarded_samples_aov_name = AiNodeGetStr(node, "discarded_samples_aov_name");
+  AiAOVRegister(bokeh->discarded_samples_aov_name, AI_TYPE_RGBA, AI_AOV_BLEND_OPACITY);
 
   // get general options
-  bokeh_data->aa_samples = AiNodeGetInt(AiUniverseGetOptions(), "AA_samples");
-  bokeh_data->xres = AiNodeGetInt(AiUniverseGetOptions(), "xres");
-  bokeh_data->yres = AiNodeGetInt(AiUniverseGetOptions(), "yres");
+  bokeh->aa_samples = AiNodeGetInt(AiUniverseGetOptions(), "AA_samples");
+  bokeh->xres = AiNodeGetInt(AiUniverseGetOptions(), "xres");
+  bokeh->yres = AiNodeGetInt(AiUniverseGetOptions(), "yres");
 
-  bokeh_data->image.clear();
-  bokeh_data->image.reserve(bokeh_data->xres * bokeh_data->yres);
+  bokeh->image.clear();
+  bokeh->image.reserve(bokeh->xres * bokeh->yres);
 }
 
 
 node_finish
 {
-  PotaBokehAOVData *bokeh_data = (PotaBokehAOVData*)AiNodeGetLocalData(node);
-  const MyCameraData *camera_data = (MyCameraData*)AiNodeGetLocalData(AiUniverseGetCamera());
+  PotaBokehAOV *bokeh = (PotaBokehAOV*)AiNodeGetLocalData(node);
+  const Camera *camera = (Camera*)AiNodeGetLocalData(AiUniverseGetCamera());
 
   // fill exr
-  std::vector<float> image(bokeh_data->yres * bokeh_data->xres * 4);
+  std::vector<float> image(bokeh->yres * bokeh->xres * 4);
   int offset = -1;
   int pixelnumber = 0;
-  int aa_square = bokeh_data->aa_samples * bokeh_data->aa_samples;
+  int aa_square = bokeh->aa_samples * bokeh->aa_samples;
 
-  for(auto i = 0; i < bokeh_data->xres * bokeh_data->yres; i++){
-    image[++offset] = bokeh_data->image[pixelnumber].r / (float)aa_square;
-    image[++offset] = bokeh_data->image[pixelnumber].g / (float)aa_square;
-    image[++offset] = bokeh_data->image[pixelnumber].b / (float)aa_square;
-    image[++offset] = bokeh_data->image[pixelnumber].a / (float)aa_square;
+  for(auto i = 0; i < bokeh->xres * bokeh->yres; i++){
+    image[++offset] = bokeh->image[pixelnumber].r / (float)aa_square;
+    image[++offset] = bokeh->image[pixelnumber].g / (float)aa_square;
+    image[++offset] = bokeh->image[pixelnumber].b / (float)aa_square;
+    image[++offset] = bokeh->image[pixelnumber].a / (float)aa_square;
     ++pixelnumber;
   }
 
 
   // replace frame numbering
-  std::string original_string = camera_data->bokeh_exr_path.c_str();
+  std::string original_string = camera->bokeh_exr_path.c_str();
   replace_frame_numbering(original_string);
-  SaveEXR(image.data(), bokeh_data->xres, bokeh_data->yres, 4, 0, original_string.c_str());
+  SaveEXR(image.data(), bokeh->xres, bokeh->yres, 4, 0, original_string.c_str());
   AiMsgWarning("[POTA] Bokeh AOV written to %s", original_string.c_str());
 
-  delete bokeh_data;
+  delete bokeh;
 }
 
 
 shader_evaluate
 {
-   PotaBokehAOVData *bokeh_data = (PotaBokehAOVData*)AiNodeGetLocalData(node);
-   MyCameraData *camera_data = (MyCameraData*)AiNodeGetLocalData(AiUniverseGetCamera());
+   PotaBokehAOV *bokeh = (PotaBokehAOV*)AiNodeGetLocalData(node);
+   Camera *camera = (Camera*)AiNodeGetLocalData(AiUniverseGetCamera());
 
    // why does this need to be in shader_evaluate to work? returns 0 in _update_?
-   bokeh_data->samples = camera_data->backward_samples * (bokeh_data->aa_samples * bokeh_data->aa_samples);
+   bokeh->samples = camera->backward_samples * (bokeh->aa_samples * bokeh->aa_samples);
 
-   const float xres = (float)bokeh_data->xres;
-   const float yres = (float)bokeh_data->yres;
+   const float xres = (float)bokeh->xres;
+   const float yres = (float)bokeh->yres;
    const float frame_aspect_ratio = xres/yres;
    AtRGBA sample_energy = AI_RGBA_ZERO;
 
    // write AOV only if in use
-   if ((sg->Rt & AI_RAY_CAMERA) && AiAOVEnabled(bokeh_data->bokeh_aov_name, AI_TYPE_RGBA))
+   if ((sg->Rt & AI_RAY_CAMERA) && AiAOVEnabled(bokeh->bokeh_aov_name, AI_TYPE_RGBA))
    {
       // figure out better way, based on:
       // distance from focus point
       // intensity of sample
-      if ((sg->out.RGBA().r > camera_data->minimum_rgb) || (sg->out.RGBA().g > camera_data->minimum_rgb) || (sg->out.RGBA().b > camera_data->minimum_rgb))
+      if ((sg->out.RGBA().r > camera->minimum_rgb) || (sg->out.RGBA().g > camera->minimum_rgb) || (sg->out.RGBA().b > camera->minimum_rgb))
       {
         // write discarded samples into separate AOV to be able to difference
-        if (AiAOVEnabled(bokeh_data->discarded_samples_aov_name, AI_TYPE_RGBA)){
-          AiAOVSetRGBA(sg, bokeh_data->discarded_samples_aov_name, sg->out.RGBA());
+        if (AiAOVEnabled(bokeh->discarded_samples_aov_name, AI_TYPE_RGBA)){
+          AiAOVSetRGBA(sg, bokeh->discarded_samples_aov_name, sg->out.RGBA());
         }
         
         sample_energy = sg->out.RGBA();
         sample_energy.a = 1.0f;
-        sample_energy /=  static_cast<float>(bokeh_data->samples);
+        sample_energy /=  static_cast<float>(bokeh->samples);
 
         // convert sample world space position to camera space
         AtMatrix world_to_camera_matrix;
@@ -144,16 +144,16 @@ shader_evaluate
         AtVector camera_space_sample_position_tmp = AiM4PointByMatrixMult(world_to_camera_matrix, sg->P);
         Eigen::Vector3d camera_space_sample_position(camera_space_sample_position_tmp.x, camera_space_sample_position_tmp.y, camera_space_sample_position_tmp.z);
          
-        for(int count=0; count<bokeh_data->samples; count++)
+        for(int count=0; count<bokeh->samples; count++)
         {
-          if(!trace_backwards( -camera_space_sample_position * 10.0, camera_data->aperture_radius, camera_data->lambda, sensor_position, camera_data->sensor_shift, camera_data))
+          if(!trace_backwards( -camera_space_sample_position * 10.0, camera->aperture_radius, camera->lambda, sensor_position, camera->sensor_shift, camera))
           {
             continue;
             --count;
           }
 
           // convert sensor position to pixel position
-          Eigen::Vector2d s(sensor_position(0) / (camera_data->sensor_width * 0.5), sensor_position(1) / (camera_data->sensor_width * 0.5) * frame_aspect_ratio);
+          Eigen::Vector2d s(sensor_position(0) / (camera->sensor_width * 0.5), sensor_position(1) / (camera->sensor_width * 0.5) * frame_aspect_ratio);
 
           const float pixel_x = ((s(0) + 1.0) / 2.0) * xres;
           const float pixel_y = ((-s(1) + 1.0) / 2.0) * yres;
@@ -171,15 +171,15 @@ shader_evaluate
           }
 
           // write sample to image
-          int pixelnumber = static_cast<int>(bokeh_data->xres * floor(pixel_y) + floor(pixel_x));
-          bokeh_data->image[pixelnumber] += sample_energy;
+          int pixelnumber = static_cast<int>(bokeh->xres * floor(pixel_y) + floor(pixel_x));
+          bokeh->image[pixelnumber] += sample_energy;
         }
       }
 
     // ideally would be cool to write to an aov but not sure if I can access the different pixels other than
     // the one related to the current sample
-    // AtRGBA aov_value = bokeh_data->image[bokeh_data->xres * (sg->y+1, sg->x+1)]; // IS THE +1 CORRECT? 
-    // AiAOVSetRGBA(sg, bokeh_data->bokeh_aov_name, aov_value);
+    // AtRGBA aov_value = bokeh->image[bokeh->xres * (sg->y+1, sg->x+1)]; // IS THE +1 CORRECT? 
+    // AiAOVSetRGBA(sg, bokeh->bokeh_aov_name, aov_value);
   }
 }
  
