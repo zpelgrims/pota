e08c76755b44db52726b760ff9718e1a79680567
zenop <zeno.pelgrims@animallogic.ca>
2018-11-29 17:54:23 -0800

    adding two test fits of double gauss

--- src/auto_generated_lens_includes/load_pt_sample_aperture.h
+++ src/auto_generated_lens_includes/load_pt_sample_aperture.h
@@ -3,5 +3,7 @@
 
 #ifdef LENS_ID_FREE
 	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/100/code/pt_sample_aperture.h"
+	#include "../../../polynomial-optics/database/lenses/9999-angenieux-double-gauss/50/code/pt_sample_aperture.h"
+	#include "../../../polynomial-optics/database/lenses/9999-angenieux-double-gauss-test-low-degree-low-coefficients/50/code/pt_sample_aperture.h"
 	#include "../../../polynomial-optics/database/lenses/2001-schneider-anamorphic/31/code/pt_sample_aperture.h"
 #endif
--- src/auto_generated_lens_includes/pota_cpp_lenses.h
+++ src/auto_generated_lens_includes/pota_cpp_lenses.h
@@ -3,5 +3,7 @@
 
 #ifdef LENS_ID_FREE
 	"angenieux_double_gauss_1953_100mm",
+	"angenieux_double_gauss_9999_50mm",
+	"angenieux_double_gauss_test_low_degree_low_coefficients_9999_50mm",
 	"schneider_anamorphic_2001_31mm",
 #endif
