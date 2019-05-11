f1537a77a9bffd0e4487143100717e2bbb4f900c
Zeno Pelgrims <zenopelgrims@gmail.com>
2019-05-11 12:17:24 -0700

    build server bash update

--- build_server.sh
+++ build_server.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# needs to be passed:
-#   username
-#   lens list
+WEBSERVER=root@78.141.196.104
 
-# placeholder
-lenses=.1001.1002
+args=("$@")
+LENSES=${args[0]}
+USER=${args[1]}
+USER_BUILD_FOLDER=${args[2]}
+DOWNLOAD_DIR=${args[3]}
 
-# create folder for user build
-DATE="$( date +"%y%m%d-%H%M" )"
-USER=$1
-USER_BUILD_FOLDER=$DATE-$USER
 mkdir -p $LENTIL_BUILD_HOME/builds/$USER_BUILD_FOLDER/bin
 
 
@@ -18,20 +15,13 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 echo ""
 echo "Setting environment variables: "
-    # green text color
-    tput setaf 2;
     export LENTIL_PATH=$CURRENT_DIR/../polynomial-optics/
     echo -e "\t LENTIL_PATH: " $LENTIL_PATH
 echo ""
 
-# reset text formatting
-tput sgr0
 
 # build the plugin
-# will need to pass the lens list
-# will need to build to custom location
 make user_build_folder=$LENTIL_BUILD_HOME/builds/$USER_BUILD_FOLDER lens_list=lenses
-
 
 # collect files into directories
 rsync -ah --progress $LENTIL_BUILD_HOME/lentil/pota/maya $LENTIL_BUILD_HOME/builds/$USER_BUILD_FOLDER/
@@ -39,5 +29,5 @@ rsync -ah --progress $LENTIL_BUILD_HOME/lentil/pota/maya $LENTIL_BUILD_HOME/buil
 # zip it up
 zip -r9 $USER_BUILD_FOLDER.zip $USER_BUILD_FOLDER
 
-# sync .zip to server hosting website
-# rsync something something something
+# sync .zip to website server
+rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress $USER_BUILD_FOLDER.zip WEBSERVER:$DOWNLOAD_DIR/
