934c6d2865cb363f900493f62a7aba97a5225698
Zeno Pelgrims <zenopelgrims@gmail.com>
2019-05-13 07:49:59 -0700

    adding 0001, 0002 fittings

--- include/auto_generated_lens_includes/load_pt_evaluate.h
+++ include/auto_generated_lens_includes/load_pt_evaluate.h
@@ -3,4 +3,9 @@
 
 #ifdef LENS_ID_FREE
 	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/49/code/pt_evaluate.h"
+	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/85/code/pt_evaluate.h"
+	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/105/code/pt_evaluate.h"
+#endif
+#ifdef LENS_ID_0002
+	#include "../../../polynomial-optics/database/lenses/1936-meyer-optik-goerlitz-primoplan/58/code/pt_evaluate.h"
 #endif
--- include/auto_generated_lens_includes/pota_cpp_lenses.h
+++ include/auto_generated_lens_includes/pota_cpp_lenses.h
@@ -3,4 +3,9 @@
 
 #ifdef LENS_ID_FREE
 	"angenieux_double_gauss_1953_49mm",
+	"angenieux_double_gauss_1953_85mm",
+	"angenieux_double_gauss_1953_105mm",
+#endif
+#ifdef LENS_ID_0002
+	"meyer_optik_goerlitz_primoplan_1936_58mm",
 #endif
