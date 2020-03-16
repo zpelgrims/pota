97a1694f18016a58137ebff331472ab265772850
zpelgrims <zenopelgrims@gmail.com>
2020-03-16 12:48:18 +0100

    new lens defines

--- include/auto_generated_lens_includes/load_lens_constants.h
+++ include/auto_generated_lens_includes/load_lens_constants.h
@@ -1,74 +1,47 @@
 // automatically generated file
 
 
-#ifdef LENS_ID_FREE
 	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/49/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/85/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/105/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1953-angenieux-double-gauss/55/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/1969-asahi-takumar/45/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1969-asahi-takumar/50/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1969-asahi-takumar/65/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1969-asahi-takumar/75/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1969-asahi-takumar/58/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1969-asahi-takumar/85/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/1970-asahi-takumar/28/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1970-asahi-takumar/50/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1970-asahi-takumar/35/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/1982-canon-retrofocus-wideangle/22/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/1956-canon-unknown/35/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1956-canon-unknown/52/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/1920-cooke-speed-panchro/40/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1920-cooke-speed-panchro/75/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1920-cooke-speed-panchro/100/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1920-cooke-speed-panchro/50/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/1948-kodak-petzval/150/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1948-kodak-petzval/105/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1948-kodak-petzval/85/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1948-kodak-petzval/65/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1948-kodak-petzval/75/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1948-kodak-petzval/58/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/1936-meyer-optik-goerlitz-primoplan/58/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1936-meyer-optik-goerlitz-primoplan/75/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/1978-minolta-fisheye/16/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1978-minolta-fisheye/22/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1978-minolta-fisheye/28/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/1971-nikon-retrofocus-wideangle/28/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1971-nikon-retrofocus-wideangle/35/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/2014-nikon-unknown/65/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/2014-nikon-unknown/40/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/2014-nikon-unknown/50/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/1900-unknown-petzval/85/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1900-unknown-petzval/100/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1900-unknown-petzval/75/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1900-unknown-petzval/65/code/lens_constants.h"
-#endif
-#ifdef LENS_ID_COMMERCIAL
 	#include "../../../polynomial-optics/database/lenses/1927-zeiss-biotar/65/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1927-zeiss-biotar/58/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1927-zeiss-biotar/85/code/lens_constants.h"
 	#include "../../../polynomial-optics/database/lenses/1927-zeiss-biotar/45/code/lens_constants.h"
-	#include "../../../polynomial-optics/database/lenses/1927-zeiss-biotar/35/code/lens_constants.h"
-#endif
