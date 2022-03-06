afcdc4d23c51c44073bed446369b74646f2730f7
zpelgrims <zenopelgrims@gmail.com>
2022-03-06 17:40:00 +0100

    comment cleanup

--- src/lentil_camera.cpp
+++ src/lentil_camera.cpp
@@ -115,112 +115,42 @@ camera_create_ray {
   output.origin = origin;
   output.dir = direction;
   output.weight = weight * camera_data->exposure;
-
-  /* 
-  NOT NEEDED FOR ARNOLD (convert rays from camera space to world space), GOOD INFO THOUGH FOR OTHER RENDER ENGINES
-  // initialise an ONB/a frame around the first vertex at the camera position along n=camera lookat direction:
-
-  view_cam_init_frame(p, &p->v[0].hit);
-      
-  for(int k=0;k<3;k++)
-  {
-  //this is the world space position of the outgoing ray:
-    p->v[0].hit.x[k] +=   camera_space_pos[0] * p->v[0].hit.a[k] 
-              + camera_space_pos[1] * p->v[0].hit.b[k] 
-              + camera_space_pos[2] * p->v[0].hit.n[k];
-
-  //this is the world space direction of the outgoing ray:
-    p->e[1].omega[k] =   camera_space_omega[0] * p->v[0].hit.a[k] 
-               + camera_space_omega[1] * p->v[0].hit.b[k]
-               + camera_space_omega[2] * p->v[0].hit.n[k];
-  }
-
-  // now need to rotate the normal of the frame, in case you need any cosines later in light transport. if not, leave out:
-  const float R = po->lens_outer_pupil_curvature_radius;
-  // recompute full frame:
-  float n[3] = {0.0f};
-  for(int k=0;k<3;k++)
-    n[k] +=   p->v[0].hit.a[k] * out[0]/R 
-        + p->v[0].hit.b[k] * out[1]/R 
-        + p->v[0].hit.n[k] * (out[2] + R)/fabsf(R);
-
-  for(int k=0;k<3;k++) p->v[0].hit.n[k] = n[k];
-
-  // also clip to valid pixel range.
-  if(p->sensor.pixel_i < 0.0f || p->sensor.pixel_i >= view_width() ||
-  p->sensor.pixel_j < 0.0f || p->sensor.pixel_j >= view_height())
-  return 0.0f;
-  */
 } 
-
-
-/*
-// given camera space scene point, return point on sensor
-inline bool trace_backwards(const AtVector sample_position, AtVector2 &sensor_position, Camera *camera)
-{
-   const float target[3] = {sample_position.x, sample_position.y, sample_position.z};
-
-   // initialize 5d light fields
-   float sensor[5] =  {0.0f, 0.0f, 0.0f, 0.0f, po->lambda};
-   float out[5] =    {0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
-   float aperture[2] =  {0.0f, 0.0f};
-
-   //randoms are always 0?
-   aperture[0] = po->random1 * po->aperture_radius;
-   aperture[1] = po->random2 * po->aperture_radius;
-
-   if(lens_lt_sample_aperture(target, aperture, sensor, out, po->lambda, camera) <= 0.0f) return false;
-
-   // crop at inward facing pupil, not needed to crop by outgoing because already done in lens_lt_sample_aperture()
-   const float px = sensor[0] + sensor[2] * po->lens_back_focal_length;
-   const float py = sensor[1] + sensor[3] * po->lens_back_focal_length; //(note that lens_focal_length is the back focal length, i.e. the distance unshifted sensor -> pupil)
-   if (px*px + py*py > po->lens_inner_pupil_radius*po->lens_inner_pupil_radius) return false;
-
-   // shift sensor
-   sensor[0] += sensor[2] * -po->sensor_shift;
-   sensor[1] += sensor[3] * -po->sensor_shift;
-
-   sensor_position.x = sensor[0];
-   sensor_position.y = sensor[1];
-
-   return true;
-}
-
 
 
 // solve Ps.xy with Po.xyz
 // fully backtraced
-camera_reverse_ray
-{
-  Camera* camera = (Camera*)AiNodeGetLocalData(node);
+// camera_reverse_ray
+// {
+//   Camera* camera = (Camera*)AiNodeGetLocalData(node);
 
-  //AiMsgInfo("Po = %f, %f, %f", Po.x, Po.y, Po.z);
+//   //AiMsgInfo("Po = %f, %f, %f", Po.x, Po.y, Po.z);
 
-  int xres = AiNodeGetInt(AiUniverseGetOptions(), "xres");
-  int yres = AiNodeGetInt(AiUniverseGetOptions(), "yres");
-  const float frame_aspect_ratio = (float)xres/(float)yres;
+//   int xres = AiNodeGetInt(AiUniverseGetOptions(), "xres");
+//   int yres = AiNodeGetInt(AiUniverseGetOptions(), "yres");
+//   const float frame_aspect_ratio = (float)xres/(float)yres;
 
-  // convert sample world space position to camera space
-  AtMatrix world_to_camera_matrix;
-  AtVector2 sensor_position;
-  AiWorldToCameraMatrix(AiUniverseGetCamera(), relative_time, world_to_camera_matrix);
-  AtVector camera_space_sample_position = AiM4PointByMatrixMult(world_to_camera_matrix, Po);
+//   // convert sample world space position to camera space
+//   AtMatrix world_to_camera_matrix;
+//   AtVector2 sensor_position;
+//   AiWorldToCameraMatrix(AiUniverseGetCamera(), relative_time, world_to_camera_matrix);
+//   AtVector camera_space_sample_position = AiM4PointByMatrixMult(world_to_camera_matrix, Po);
 
-  if( trace_backwards( -camera_space_sample_position * 10.0, sensor_position, camera) )
-  {
-    AtVector2 s(sensor_position.x / (po->sensor_width * 0.5), 
-          sensor_position.y / (po->sensor_width * 0.5) * frame_aspect_ratio);
+//   if( trace_backwards( -camera_space_sample_position * 10.0, sensor_position, camera) )
+//   {
+//     AtVector2 s(sensor_position.x / (po->sensor_width * 0.5), 
+//           sensor_position.y / (po->sensor_width * 0.5) * frame_aspect_ratio);
 
-    AiMsgInfo("sensorposition: %f \t %f", s.x, s.y);
+//     AiMsgInfo("sensorposition: %f \t %f", s.x, s.y);
 
-    Ps.x = s.x;
-    Ps.y = s.y;
+//     Ps.x = s.x;
+//     Ps.y = s.y;
 
-    return true;
-  } else {
-    return false;
-  }
-}*/
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
 
 // approximation using pinhole camera FOV
